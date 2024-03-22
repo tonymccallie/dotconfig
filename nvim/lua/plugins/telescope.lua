c5528d135e1455a2914e325f4d7121615f32e659
Tony McCallie <tonymccallie@gmail.com>
2024-03-22 07:48:16 -0600

    chanded to codeium

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
       {
         "<c-p>",
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
+        desc = "Find File",
       },
   },
   -- change some options
